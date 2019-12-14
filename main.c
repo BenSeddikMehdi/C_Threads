692264ab217f81c47cb26b24d9eb7ce5d6bc40d5
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-15 00:51:00 +0100

    Passing arguments to and get result from threads

--- main.c
+++ main.c
@@ -2,31 +2,35 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 void* myTurn(void* arg) {
-    uint8_t i = 0;
-    while (i < 3) {
+    uint8_t i = 0, *i_ptr = (uint8_t*) malloc(sizeof(uint8_t));
+    *i_ptr = 10;
+    while (i < 7) {
         sleep(1);
-        printf("Hello from the first Thread %p\n", (uint8_t*) arg);
+        printf("Hello from the first Thread = %d ; %d \n", i, *i_ptr);
         ++i;
+        ++*i_ptr;
     }
-    return NULL;
+    return i_ptr;
 }
 void yourTurn() {
     uint8_t i = 0;
     while (i < 3) {
         sleep(2);
-        printf("******Second Thread\n");
+        printf("******Second Thread = %d ;\n", i);
         ++i;
     }
 }
 
 int main() {
     pthread_t pthread;
-    uint8_t num = 123;
-    pthread_create(&pthread, NULL, myTurn, &num);
+    uint8_t* result;
+    pthread_create(&pthread, NULL, myTurn, NULL);
     //myTurn();
     yourTurn();
-    //pthread_join(pthread, NULL);
+    pthread_join(pthread, (void*) &result);
+    printf("After the threads are finished num = %d", *result);
     return 0;
 }
