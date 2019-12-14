1b43f366e4a7a6eaea64e3eb180f740e0e3a05d9
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-14 20:31:13 +0100

    Trying to implement two threads

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <stdint.h>
+
+void* myTurn(void* arg) {
+    int8_t i = 0;
+    while (i < 5) {
+        sleep(1);
+        printf("My turn\n");
+        ++i;
+    }
+    return NULL;
+}
+void yourTurn() {
+    int8_t i = 0;
+    while (i < 5) {
+        sleep(2);
+        printf("Your turn\n");
+        ++i;
+    }
+}
 
 int main() {
     pthread_t pthread;
-    printf("Hello, World!\n");
+    pthread_create(&pthread, NULL, myTurn, NULL);
+    //myTurn();
+    yourTurn();
+    pthread_join(pthread, NULL);
     return 0;
 }
