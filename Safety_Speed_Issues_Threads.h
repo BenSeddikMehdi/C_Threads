661bf68ab5f9bf9a9bc5ab2979782c55e198af64
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-15 18:52:26 +0100

    Safety and Speed issues with threads

--- Safety_Speed_Issues_Threads.h
+++ Safety_Speed_Issues_Threads.h
@@ -5,4 +5,13 @@
 #ifndef C_THREADS_SAFETY_SPEED_ISSUES_THREADS_H
 #define C_THREADS_SAFETY_SPEED_ISSUES_THREADS_H
 
+#define BIG 1000000000UL
+
+void countUpToBIG(uint32_t* counter) {
+     *counter = 0;
+    for (uint32_t i = 0; i < BIG; ++i) {
+        (*counter)++;
+    }
+}
+
 #endif //C_THREADS_SAFETY_SPEED_ISSUES_THREADS_H
--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include "Safety_Speed_Issues_Threads.h"
 
 int main() {
-
+    uint32_t counter = 0;
+    clock_t begin = clock();
+    countUpToBIG(&counter);
+    countUpToBIG(&counter);
+    clock_t end = clock();
+    printf("Done. counter %u\n", counter);
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("time spent: %f", time_spent);
     return 0;
 }
